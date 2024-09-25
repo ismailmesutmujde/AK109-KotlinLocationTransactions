5eccb285bde48afdacd241593060a59c6cdce80a
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-25 18:28:37 +0300

    updated: permission control

--- app/src/main/java/com/ismailmesutmujde/kotlinlocationtransactions/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinlocationtransactions/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.ismailmesutmujde.kotlinlocationtransactions
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.ismailmesutmujde.kotlinlocationtransactions.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bindingMain : ActivityMainBinding
+    private var permissionControl = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingMain = ActivityMainBinding.inflate(layoutInflater)
@@ -14,8 +22,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         bindingMain.buttonGetLocation.setOnClickListener {
+            permissionControl = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            if (permissionControl != PackageManager.PERMISSION_GRANTED) {
+                // if permission is not approved
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 100)
+            } else {
+                // if permission is approved
 
+
+            }
         }
+    }
 
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if(requestCode == 100) {
+            permissionControl = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(applicationContext, "Permission granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, "Permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
