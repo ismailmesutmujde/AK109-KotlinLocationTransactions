48f26304d7c76e681c3607be70d15b3a3448b9a5
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-25 18:15:48 +0300

    updated: ui design and pre-installation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.ismailmesutmujde.kotlinlocationtransactions"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.ismailmesutmujde.kotlinlocationtransactions"
         minSdk = 24
@@ -45,4 +49,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.google.android.gms:play-services-location:21.3.0")
 }
--- app/src/main/java/com/ismailmesutmujde/kotlinlocationtransactions/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinlocationtransactions/MainActivity.kt
@@ -2,11 +2,20 @@ package com.ismailmesutmujde.kotlinlocationtransactions
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlinlocationtransactions.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMain : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMain.root
+        setContentView(view)
+
+        bindingMain.buttonGetLocation.setOnClickListener {
+
+        }
 
     }
 }
